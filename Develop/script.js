35ca2160d4cc2b401a6ad9e83bb0155bbf9b0c9f
PGCranford <PGCranford@gmail.com>
2022-05-18 17:55:47 -0400

    intitial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,64 @@
 // Assignment code here
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const LETTERS = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+const numbers = "1234567890";
+const chars = "!.@.#,&,(,),<,>,%";
+
+
+
+
+
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//setting password length
 
-  passwordText.value = password;
+// var passwordLength = function getRandomInt(min, max) {
+//   min = Math.ceil(8);
+//   max = Math.floor(128);
+//   return Math.floor(mathRandom() * (max - min + 1) + min);
+// }
+// var passwordLength = "";
+// while (passwordLength === "" || passwordLength === null) {
+//   passwordLength = prompt('How long would you like your password to be?');
+// }
+
+
+//asking if lowercase letters should be included
+
+var lowercasePrompt = window.confirm("Would you like to include lowercase letters?");
+
+if (confirm("OK") == true) {
+
+  function 
+
+
 
 }
 
+//asking if uppercase letters should be included
+
+//asking if numbers should be included
+
+//asking if special characters should be included
+
+
+
+
+
+
+
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// document.getElementById("btn").generateBtn.addEventListener("click");
