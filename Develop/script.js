1690d118a3bf07f9c9824033dfb1021e21030c7e
PGCranford <PGCranford@gmail.com>
2022-05-18 19:19:00 -0400

    adding window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,12 @@
 // Assignment code here
-const letters = "abcdefghijklmnopqrstuvwxyz";
-const LETTERS = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
-const numbers = "1234567890";
-const chars = "!.@.#,&,(,),<,>,%";
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var LETTERS = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+var numbers = "1234567890";
+var characters = "!.@.#,&,(,),<,>,%";
+
+var password = {
+  x + 
+}
 
 
 
@@ -15,15 +19,15 @@ const chars = "!.@.#,&,(,),<,>,%";
 
 //setting password length
 
-// var passwordLength = function getRandomInt(min, max) {
-//   min = Math.ceil(8);
-//   max = Math.floor(128);
-//   return Math.floor(mathRandom() * (max - min + 1) + min);
-// }
-// var passwordLength = "";
-// while (passwordLength === "" || passwordLength === null) {
-//   passwordLength = prompt('How long would you like your password to be?');
-// }
+var passwordLength = function getRandomInt(min, max) {
+  min = Math.ceil(8);
+  max = Math.floor(128);
+  return Math.floor(mathRandom() * (max - min + 1) + min);
+}
+var passwordLength = "";
+while (passwordLength === "" || passwordLength === null) {
+  passwordLength = prompt('How long would you like your password to be?');
+}
 
 
 //asking if lowercase letters should be included
@@ -31,18 +35,55 @@ const chars = "!.@.#,&,(,),<,>,%";
 var lowercasePrompt = window.confirm("Would you like to include lowercase letters?");
 
 if (confirm("OK") == true) {
-
-  function 
-
+  letters = { a };
+}
+else {
+  (confirm("cancel") == false)
+  letters = { b };
 
 
 }
 
+
 //asking if uppercase letters should be included
+
+var lowercasePrompt = window.confirm("Would you like to include uppercase letters?");
+
+if (confirm("OK") == true) {
+  LETTERS = { c };
+}
+else {
+  (confirm("cancel") == false)
+  LETTERS = { d };
+}
+
 
 //asking if numbers should be included
 
+var lowercasePrompt = window.confirm("Would you like to include numbers?");
+
+if (confirm("OK") == true) {
+  numbers = { d };
+}
+else {
+  (confirm("cancel") == false)
+  numbers = { e };
+}
+
+
+
 //asking if special characters should be included
+
+var lowercasePrompt = window.confirm("Would you like to include special characters?");
+
+if (confirm("OK") == true) {
+  characters = { f };
+}
+else {
+  (confirm("cancel") == false)
+  characters = { g };
+
+
 
 
 
