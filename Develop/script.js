3f833c3d10f8ca94250762d156f03ddba7dde893
PGCranford <PGCranford@gmail.com>
2022-05-20 18:33:33 -0400

    total rebuild

--- Develop/script.js
+++ Develop/script.js
@@ -1,119 +1,118 @@
-// Assignment code here
-var letters = "abcdefghijklmnopqrstuvwxyz";
-var LETTERS = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
-var numbers = "1234567890";
-var characters = "!.@.#,&,(,),<,>,%";
+function generatePassword() {
+  // Assignment code here
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var LETTERS = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+  var numbers = "1234567890";
+  var characters = "!.@.#,&,(,),<,>,%";
 
 
-// Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
+  // Get references to the #generate element
+
 
   //asking if lowercase letters should be included
-
-{
-    var lowercasePrompt = window.confirm("Would you like to include lowercase letters?");
-    
-    if (confirm("OK") == true) {
-      while (true){
-
-        lowercase = letters;
-        }
-
-    }else {
-      (confirm("cancel") == false)
-
-     } 
-
-  
-  
-
-      //asking if uppercase letters should be included
-
-   var uppercasePrompt = window.confirm("Would you like to include uppercase letters?");
-
-        if (confirm("OK") == true) {
-          while (true){
-            uppercase = LETTERS
-            }
-        } else {
-            (confirm("cancel") == false)
-        } 
+  var userSelections = ""
 
 
-         //asking if numbers should be included
+  var lowercasePrompt = window.confirm("Would you like to include lowercase letters?")
 
-     var numbersPrompt = window.confirm("Would you like to include numbers?");
+  if (lowercasePrompt) {
+    userSelections = userSelections + letters
 
-      if (confirm("OK") == true) {
-        while (true){
-          numerical = numbers 
-         }
-      }
-        else {
-         (confirm("cancel") == false)
-
-        }
-   
+  }
 
 
-       //asking if special characters should be included
-
-        var characterPrompt = window.confirm("Would you like to include special characters?");
-
-      if (confirm("OK") == true) {
-        while  (true) {
-          info = characters
-         }
-      }
-      else {
-        (confirm("cancel") == false)
-      }
-     
-
-  
-
-      //setting password length
-  
-      var passwordLength = window.prompt('How long would you like your password to be?');
-
-      var x = parseInt("8");
-      var y = parseInt("128");
-
-      if (passwordLength => x || passwordLength <= y) {
-        window.alert("Good choice.");
-        passwordLength = input
-      }
-      else {
-        ("Please try again")
-      }
-
-}
-      //  function generatePassword(){
-      //  for (var i=0; i = passwordLength){
-      //   Math.floor(Math.random() * lowercase.length);
-      //   Math.floor(Math.random() * uppercase.length);
-      //   Math.floor(Math.random() * numerical.length);
-      //   Math.floor(Math.random() * info.length);
-      //  }
-
-  // var generatePassword = function () {
 
 
+
+  //asking if uppercase letters should be included
+
+  var uppercasePrompt = window.confirm("Would you like to include uppercase letters?");
+
+  if (confirm("OK") == true) {
+    while (true) {
+      uppercase = LETTERS
+      break;
+    }
+  } else {
+    (confirm("cancel") == false)
+  }
+
+
+  //asking if numbers should be included
+
+  var numbersPrompt = window.confirm("Would you like to include numbers?");
+
+  if (confirm("OK") == true) {
+    while (true) {
+      numerical = numbers
+      break;
+    }
+  }
+  else {
+    (confirm("cancel") == false)
+
+  }
+
+
+
+  //asking if special characters should be included
+
+  var characterPrompt = window.confirm("Would you like to include special characters?");
+
+  if (confirm("OK") == true) {
+    while (true) {
+      info = characters
+      break;
+    }
+  }
+  else {
+    (confirm("cancel") == false)
+  }
+
+
+
+
+  //setting password length
+
+  var passwordLength = window.prompt('How long would you like your password to be?');
+  passwordLength = Number.parseInt(passwordLength, 10)
+
+  var minPasswordlength = 8;
+  var maxPasswordlength = 128;
+
+  var password = ""
+
+  if (!Number.isNaN(passwordLength) && passwordLength >= minPasswordlength && passwordLength <= maxPasswordlength) {
+
+    while (password.length < passwordLength) {
+      var randomIndex = Math.floor(Math.random() * userSelections.length);
+
+      var randomCharacter = userSelections.charAt(randomIndex);
+      password = password + randomCharacter
+    }
+
+
+
+  }
+  else {
+    window.alert("Please try again")
+  }
   // // Write password to the #password input
-  // function writePassword() {
-
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
 
 
-  //  
 
-  //   }
-
-  //  
+  return password
+}
 
 
-  // Add event listener to generate button
-  // document.getElementById("btn").generateBtn.addEventListener("click")
 
+
+
+
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", function () {
+  var password = generatePassword()
+  var passwordDislplay = document.querySelector("#password")
+  passwordDislplay.textContent = password
+})
 
