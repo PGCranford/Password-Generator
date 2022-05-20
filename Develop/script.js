4ef711aa4b30e04d832785cf0aec212e3818add1
PGCranford <PGCranford@gmail.com>
2022-05-20 15:35:37 -0400

    adding while loops

--- Develop/script.js
+++ Develop/script.js
@@ -4,102 +4,116 @@ var LETTERS = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
 var numbers = "1234567890";
 var characters = "!.@.#,&,(,),<,>,%";
 
-var password = {
-  x + 
-}
-
-
-
-
-
-
 
 // Get references to the #generate element
 //var generateBtn = document.querySelector("#generate");
 
-//setting password length
+  //asking if lowercase letters should be included
 
-var passwordLength = function getRandomInt(min, max) {
-  min = Math.ceil(8);
-  max = Math.floor(128);
-  return Math.floor(mathRandom() * (max - min + 1) + min);
+{
+    var lowercasePrompt = window.confirm("Would you like to include lowercase letters?");
+    
+    if (confirm("OK") == true) {
+      while (true){
+
+        lowercase = letters;
+        }
+
+    }else {
+      (confirm("cancel") == false)
+
+     } 
+
+  
+  
+
+      //asking if uppercase letters should be included
+
+   var uppercasePrompt = window.confirm("Would you like to include uppercase letters?");
+
+        if (confirm("OK") == true) {
+          while (true){
+            uppercase = LETTERS
+            }
+        } else {
+            (confirm("cancel") == false)
+        } 
+
+
+         //asking if numbers should be included
+
+     var numbersPrompt = window.confirm("Would you like to include numbers?");
+
+      if (confirm("OK") == true) {
+        while (true){
+          numerical = numbers 
+         }
+      }
+        else {
+         (confirm("cancel") == false)
+
+        }
+   
+
+
+       //asking if special characters should be included
+
+        var characterPrompt = window.confirm("Would you like to include special characters?");
+
+      if (confirm("OK") == true) {
+        while  (true) {
+          info = characters
+         }
+      }
+      else {
+        (confirm("cancel") == false)
+      }
+     
+
+  
+
+      //setting password length
+  
+      var passwordLength = window.prompt('How long would you like your password to be?');
+
+      var x = parseInt("8");
+      var y = parseInt("128");
+
+      if (passwordLength => x || passwordLength <= y) {
+        window.alert("Good choice.");
+        passwordLength = input
+      }
+      else {
+        ("Please try again")
+      }
+
 }
-var passwordLength = "";
-while (passwordLength === "" || passwordLength === null) {
-  passwordLength = prompt('How long would you like your password to be?');
-}
+      //  function generatePassword(){
+      //  for (var i=0; i = passwordLength){
+      //   Math.floor(Math.random() * lowercase.length);
+      //   Math.floor(Math.random() * uppercase.length);
+      //   Math.floor(Math.random() * numerical.length);
+      //   Math.floor(Math.random() * info.length);
+      //  }
+
+  // var generatePassword = function () {
 
 
-//asking if lowercase letters should be included
+  // // Write password to the #password input
+  // function writePassword() {
 
-var lowercasePrompt = window.confirm("Would you like to include lowercase letters?");
-
-if (confirm("OK") == true) {
-  letters = { a };
-}
-else {
-  (confirm("cancel") == false)
-  letters = { b };
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
 
-}
+  //  
+
+  //   }
+
+  //  
 
 
-//asking if uppercase letters should be included
-
-var lowercasePrompt = window.confirm("Would you like to include uppercase letters?");
-
-if (confirm("OK") == true) {
-  LETTERS = { c };
-}
-else {
-  (confirm("cancel") == false)
-  LETTERS = { d };
-}
+  // Add event listener to generate button
+  // document.getElementById("btn").generateBtn.addEventListener("click")
 
 
-//asking if numbers should be included
-
-var lowercasePrompt = window.confirm("Would you like to include numbers?");
-
-if (confirm("OK") == true) {
-  numbers = { d };
-}
-else {
-  (confirm("cancel") == false)
-  numbers = { e };
-}
-
-
-
-//asking if special characters should be included
-
-var lowercasePrompt = window.confirm("Would you like to include special characters?");
-
-if (confirm("OK") == true) {
-  characters = { f };
-}
-else {
-  (confirm("cancel") == false)
-  characters = { g };
-
-
-
-
-
-
-
-
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// Add event listener to generate button
-// document.getElementById("btn").generateBtn.addEventListener("click");
